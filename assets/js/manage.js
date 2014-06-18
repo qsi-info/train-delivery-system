d2669afcace48e38abed9f3f0f40e93871695342
Alexandre Page <alexgroove24@gmail.com>
2014-06-18 13:33:33 -0400

    fix report page. Now open in new window

--- assets/js/manage.js
+++ assets/js/manage.js
@@ -331,10 +331,34 @@
 	})
 
 
+	$('.report-transfersheet').on('click', function (e) {
+		e.preventDefault();
+		socket.get($(this).attr('href'), function (response) {
+			var url = 'http://parachemsrv07/Reports/Pages/Report.aspx?ItemPath=%2fTransferSheet';
+			Utils.popupWindow(url, 1200, 800);
+			$('#printModal').modal('toggle');
+		})
+	})
+
+
+	$('.report-inspectionsheet').on('click', function (e) {
+		e.preventDefault();
+		socket.get($(this).attr('href'), function (response) {
+			var url = 'http://parachemsrv07/Reports/Pages/Report.aspx?ItemPath=%2fInspectionSheets';
+			Utils.popupWindow(url, 1200, 800);
+			$('#printModal').modal('toggle');
+		})
+	})
+
+
 	$('.report-sealsheet-link').on('click', function (e) {
 		e.preventDefault();
 		var firstSeal = window.prompt('Entrez le premier numero de seal', '4989501');
-		window.location = $(this).attr('href') + '/' + firstSeal;
+		socket.get($(this).attr('href') + '/' + firstSeal, function (response) {
+			var url = 'http://parachemsrv07/Reports/Pages/Report.aspx?ItemPath=%2fSealSheet';
+			Utils.popupWindow(url, 1200, 800);
+			$('#printModal').modal('toggle');
+		})
 	});
 
 	// -----------------------
---
+++ assets/js/utils.js
@@ -0,0 +1,27 @@
+window.Utils = window.Utils || {};
+
+Utils.popupWindow = function (url, width, height) {
+	var newwindow;
+  var  screenX     = typeof window.screenX     != 'undefined' ? window.screenX     : window.screenLeft,
+       screenY     = typeof window.screenY     != 'undefined' ? window.screenY     : window.screenTop,
+       outerWidth  = typeof window.outerWidth  != 'undefined' ? window.outerWidth  : document.body.clientWidth,
+       outerHeight = typeof window.outerHeight != 'undefined' ? window.outerHeight : (document.body.clientHeight - 22),
+       width    = width,
+       height   = height,
+       left     = parseInt(screenX + ((outerWidth - width) / 2), 10),
+       top      = parseInt(screenY + ((outerHeight - height) / 2.5), 10),
+       features = (
+          'width='   + width +
+          ',height=' + height +
+          ',left='   + left +
+          ',top='    + top +
+          ', scrollbars=no, menubar=no, toolbar=no, status=no, location=no, directories=no'
+        );
+
+  newwindow=window.open(url,'facebook',features);
+
+  if (window.focus) {
+    newwindow.focus();
+  }
+  return newwindow;
+}
