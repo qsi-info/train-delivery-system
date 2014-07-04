735dfd2f527be221c392227bb95a1ba85e6639ac
Alexandre Page <alexandre.page.work@gmail.com>
2014-07-04 15:38:27 -0400

    match sharepoint styles

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,7 +10,7 @@
 
 module.exports.bootstrap = function (cb) {
 
-	sails.config.appName = 'Train Delivery System v1.0';
+	sails.config.appName = 'Gestion des Trains';
 
   // It's very important to trigger this callack method when you are finished 
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
